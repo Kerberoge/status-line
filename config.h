ee9f5d6b8f7905b0406be88a88dea4986a1948ab
kerberoge <sjoerdenjh@gmail.com>
2025-03-11 00:42:24 +0100

    Moved sleep indicator back to the far left

--- config.h
+++ config.h
@@ -9,11 +9,11 @@
 #define SEP		"     "
 
 struct element elements[] = {
+	{ sleep_state,	"",											/* normal */
+					"^fg(" FG_WN ")INSOMNIA^fg()" },			/* sleep inhibition */
 	{ volume,	"^fg(" FG_AC ")V^fg() %.0f%%",					/* normal */
 				"^fg(" FG_AC ")V^fg() muted" },					/* muted */
 	{ kb_layout,	"^fg(" FG_AC ")K^fg() %s" },
-	{ sleep_state,	"",											/* normal */
-					"^fg(" FG_WN ")INSOMNIA^fg()" },			/* sleep inhibition */
 	{ memory,	"^fg(" FG_AC ")M^fg() %.0fM",					/* < 1GB */
 				"^fg(" FG_AC ")M^fg() %.1fG" },					/* > 1GB */
 	{ cpu,		"^fg(" FG_AC ")C^fg() %02.0f%%",						/* normal */

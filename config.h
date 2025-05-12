57a1f1aa7a2c7cfa57d42be65a26ae9db1e79ece
kerberoge <sjoerdenjh@gmail.com>
2025-05-12 21:44:45 +0200

    Replaced signaling mechanism with poll()
    - inotify is now used to monitor file for sleep_state()
    - volume() now writes to an fd to notify the main loop that it should print immediately,
      instead of sending a kill signal to its own process

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
-#define HWMON_PATH		"/sys/class/hwmon/hwmon2/temp1_input"
-#define BATTERY_PATH	"/sys/class/power_supply/BAT0"
-#define WIFI_DEVICE		"wlo1"
+#define HWMON_PATH			"/sys/class/hwmon/hwmon2/temp1_input"
+#define BATTERY_PATH		"/sys/class/power_supply/BAT0"
+#define WIFI_DEVICE			"wlo1"
+#define SLEEP_STATE_PATH	"/tmp/inhibit_sleep"
 
 #define FG_AC	"37bf7c"
 #define FG_WN	"ffff00"
@@ -9,20 +10,20 @@
 #define SEP		"     "
 
 struct element elements[] = {
-	{ volume,	"^fg(" FG_AC ")V^fg() %.0f%%",					/* normal */
-				"^fg(" FG_AC ")V^fg() muted" },					/* muted */
-	{ sleep_state,	"",											/* normal */
-					"^fg(" FG_WN ")INSOMNIA^fg()" },			/* sleep inhibition */
-	{ memory,	"^fg(" FG_AC ")M^fg() %.0fM",					/* < 1GB */
-				"^fg(" FG_AC ")M^fg() %.1fG" },					/* > 1GB */
-	{ cpu,		"^fg(" FG_AC ")C^fg() %02.0f%%",						/* normal */
-				"^fg(" FG_AC ")C^fg() ^fg(" FG_UR ")%02.0f%%^fg()" },	/* high */
-	{ temperature,	"^fg(" FG_AC ")T^fg() %d°C",						/* normal */
-					"^fg(" FG_AC ")T^fg() ^fg(" FG_UR ")%d°C^fg()" },	/* high */
-	{ battery,	"^fg(" FG_AC ")B^fg() %u%%",						/* normal */
-				"^fg(" FG_AC ")Ch^fg() %u%%",						/* charging */
-				"^fg(" FG_AC ")B^fg() ^fg(" FG_UR ")%u%%^fg()" },	/* low battery */
-	{ wifi,		"^fg(" FG_AC ")W^fg() %s",							/* connected */
-				"^fg(" FG_UR ")disconnected^fg()" },				/* disconnected */
-	{ date,		"^fg(" FG_AC ")D^fg() %s %02d-%02d  %d:%02d" }
+	{ volume,		0,	"^fg(" FG_AC ")V^fg() %.0f%%",							/* normal */
+						"^fg(" FG_AC ")V^fg() muted" },							/* muted */
+	{ sleep_state,	0,	"",														/* normal */
+						"^fg(" FG_WN ")INSOMNIA^fg()" },						/* sleep inhibited */
+	{ memory,		1,	"^fg(" FG_AC ")M^fg() %.0fM",							/* < 1GB */
+						"^fg(" FG_AC ")M^fg() %.1fG" },							/* > 1GB */
+	{ cpu,			1,	"^fg(" FG_AC ")C^fg() %02.0f%%",						/* normal */
+						"^fg(" FG_AC ")C^fg() ^fg(" FG_UR ")%02.0f%%^fg()" },	/* high */
+	{ temperature,	1,	"^fg(" FG_AC ")T^fg() %d°C",							/* normal */
+						"^fg(" FG_AC ")T^fg() ^fg(" FG_UR ")%d°C^fg()" },		/* high */
+	{ battery,		1,	"^fg(" FG_AC ")B^fg() %u%%",							/* normal */
+						"^fg(" FG_AC ")Ch^fg() %u%%",							/* charging */
+						"^fg(" FG_AC ")B^fg() ^fg(" FG_UR ")%u%%^fg()" },		/* low battery */
+	{ wifi,			1,	"^fg(" FG_AC ")W^fg() %s",								/* connected */
+						"^fg(" FG_AC ")W^fg(" FG_UR ") disconnected^fg()" },	/* disconnected */
+	{ date,			1,	"^fg(" FG_AC ")D^fg() %s %02d-%02d  %d:%02d" }
 };
